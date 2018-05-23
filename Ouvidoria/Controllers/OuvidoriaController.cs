1d2699b7280ae4ac96d4566964938a8e3bc27188
Rhuan Matheus Drescher <rhuan.drescher@meta.com.br>
2018-05-23 11:14:22 -0300

    Formulario adicionado e configurado no controller Ouvidoria

--- Ouvidoria/Controllers/OuvidoriaController.cs
+++ Ouvidoria/Controllers/OuvidoriaController.cs
@@ -1,15 +1,36 @@
-﻿using System.Web.Mvc;
+﻿using Ouvidoria.Models;
+using System.Web.Mvc;
 
 
 namespace Ouvidoria.Controllers
 {
-    
-    
+
+
     public class OuvidoriaController : Controller
     {
+        private OuvidoriaContext db = new OuvidoriaContext();
+
         public ActionResult Index()
         {
+            ViewBag.idEventoTipo = new SelectList(db.EventoTipo, "id", "Tipo");
+            ViewBag.idUsuario = new SelectList(db.Usuario, "id", "Nome");
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "id,Titulo,Descricao,idEventoTipo,idUsuario")] Evento evento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Evento.Add(evento);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idEventoTipo = new SelectList(db.EventoTipo, "id", "Tipo", evento.idEventoTipo);
+            ViewBag.idUsuario = new SelectList(db.Usuario, "id", "Nome", evento.idUsuario);
+            return View(evento);
         }
     }
 }
