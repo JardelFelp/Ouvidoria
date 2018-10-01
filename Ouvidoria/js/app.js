df2881cfb24afe6f7cc48ff3590d70b3147b348e
Jardel Felipe Knirsch <jardelfelipeknirsch@gmail.com>
2018-10-01 18:10:08 -0300

    Ajustes finais nas telas de usuário

--- Ouvidoria/js/app.js
+++ Ouvidoria/js/app.js
@@ -1,4 +1,15 @@
 ﻿$(document).ready(function () {
-    
+    const itens = $('#acessos li.op');
 
+    if (itens.length) {
+        for (let i = 0; i < itens.length; i++) {
+            const url = $(itens[i]).attr('url');
+
+            if (window.location.pathname.includes(url)) {
+                $(itens[i]).addClass('active');
+            } else {
+                $(itens[i]).removeClass('active');
+            }
+        }
+    }
 });
